a722950fa4aaecb29794e92fdffab0bbfbf91bb7
wyattfry <wyattfry@gmail.com>
2018-02-16 16:28:25 -0800

    Implements the database schema and save method

--- database/index.js
+++ database/index.js
@@ -3,14 +3,33 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner: String,
+  html_url: String,
+  description: String,
+  fork: Boolean,
+  updated_at: Date,
+  stargazers_count: Number,
+  forks_count: Number,
 });
 
+// models are classes with which documents (rows) are constructed.
 let Repo = mongoose.model('Repo', repoSchema);
+//                        what is this first argument?
 
-let save = (/* TODO */) => {
+// let save = (/* TODO */) => {
+let save = (repo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let repoToSave = new Repo();
+  repoToSave.save((err, repoToSave) => {
+    //                  what is this 2nd arg?
+    if (err) {
+      console.log('Repo could not be saved.');
+    }
+  });
 }
 
 module.exports.save = save;
